7ea378cae85e5016f3ed63f891654b738aa9de15
yhonatantapias <yhonatantapias@destiny.ws>
2024-02-16 13:16:57 -0500

    backup bd

--- bd/parqueadero_con_datos.sql
+++ bd/parqueadero_con_datos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-02-2024 a las 01:27:30
+-- Tiempo de generación: 07-01-2024 a las 08:41:42
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -61,8 +61,33 @@ CREATE TABLE `moto` (
   `valor_cobrado` int(11) NOT NULL,
   `fecha_salida` datetime NOT NULL,
   `estado` int(11) NOT NULL,
-  `cascos` text NOT NULL,
-  `ubicacion` text NOT NULL
+  `cascos` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `moto`
+--
+
+INSERT INTO `moto` (`id`, `placa`, `descripcion`, `fecha_ingreso`, `valor_cobrado`, `fecha_salida`, `estado`, `cascos`) VALUES
+(1, 'cpn67e', 'sin placa', '2024-01-07 01:09:10', 1800, '2024-01-07 02:28:37', 0, '1'),
+(2, 'hjq31e', '', '2024-01-07 02:26:53', 0, '0000-00-00 00:00:00', 1, '4'),
+(3, 'ncp 67e', 'fff', '2024-01-07 02:35:20', 0, '0000-00-00 00:00:00', 1, '2'),
+(4, 'ooo', '222', '2024-01-07 02:36:14', 0, '0000-00-00 00:00:00', 1, '111');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros_parqueadero`
+--
+
+CREATE TABLE `registros_parqueadero` (
+  `id` int(11) NOT NULL,
+  `placa` varchar(20) NOT NULL,
+  `fecha_entrada` datetime NOT NULL,
+  `observaciones` text DEFAULT NULL,
+  `fecha_salida` datetime DEFAULT NULL,
+  `tiempo_duracion` int(11) DEFAULT NULL,
+  `valor_cobrado` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -88,6 +113,12 @@ ALTER TABLE `moto`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `registros_parqueadero`
+--
+ALTER TABLE `registros_parqueadero`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -107,6 +138,12 @@ ALTER TABLE `ingresos`
 -- AUTO_INCREMENT de la tabla `moto`
 --
 ALTER TABLE `moto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `registros_parqueadero`
+--
+ALTER TABLE `registros_parqueadero`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
