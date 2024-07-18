fede686074dafb941e55e7efff3d244f9914c51a
yhonatantapias <yhonatantapias@destiny.ws>
2024-07-17 20:09:30 -0500

    información de las fechas de consulta historico

--- historial.php
+++ historial.php
@@ -28,11 +28,11 @@
 						</td>
 						<td>
 							<a href="lavadas.php"  class="btn btn-primary" style="width: 100%;"><span
-									class="	glyphicon glyphicon-tint"></span> Lavadas</a> 
+									class="	glyphicon glyphicon-tint"></span> Lavadas</a>
 						</td>
 						<td>
 							<a href="ingresos.php"  class="btn btn-warning" style="width: 100%;"><span
-									class="	glyphicon glyphicon-usd"></span> Ingresos</a> 
+									class="	glyphicon glyphicon-usd"></span> Ingresos</a>
 						</td>
 						<td>
 							<a href="gastos.php"  class="btn btn-primary" style="width: 100%;"><span
@@ -52,7 +52,7 @@
 					</tr>
 				</table>
 
-				
+
 			</div>
 
 
@@ -60,19 +60,25 @@
 
 
 			<form method="POST" action="#">
-					<!--div class="col-lg-4">
-						<input type="date" class="form-control" id="fechai" name="fechai"  require 
-							placeholder="Fecha incio" >
-					</div -->
+
 					<div class="col-lg-4">
-					<label>Seleccione la fecha:</label>
-						<input type="date"   class="form-control" id="fechaf" name="fechaf"  require
-							placeholder="Fecha fin" value="<?php if (isset($_POST['fechaf'])) {
-								echo $_POST['fechaf'];
-							} ?>" >
+
+			<label>Seleccione la fecha Incio:</label>
+						<input type="date" class="form-control" id="fechai" name="fechai"  require
+							placeholder="Fecha incio" value="<?php if (isset($_POST['fechai'])) {
+    echo $_POST['fechai'];
+}?>">
 					</div>
 					<div class="col-lg-4">
-					<br/>					
+						
+			<label>Seleccione la fecha Fin:</label>
+						<input type="date"   class="form-control" id="fechaf" name="fechaf"  require
+							placeholder="Fecha fin" value="<?php if (isset($_POST['fechaf'])) {
+    echo $_POST['fechaf'];
+}?>" >
+					</div>
+					<div class="col-lg-4">
+					<br/>
 						<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-check"></span>
 							Consultar</button>
 
@@ -81,80 +87,76 @@
 				<br><br>
 
 			<div style="height:30px;"></div>
-			<?php 
-			include('conn.php');
-			
-			
-			// Establecer la zona horaria a Colombia
-                date_default_timezone_set('America/Bogota');
+			<?php
+include 'conn.php';
 
-				$dia = date('d'); // Día actual (en formato de dos dígitos, con ceros iniciales si es necesario)
-                $mes = date('m'); // Mes actual (en formato de dos dígitos, con ceros iniciales si es necesario)
-                $ano = date('Y'); // Año actual (en formato de cuatro dígitos)
-                
-                $fecha =  $ano."-".$mes."-". $dia;
-                //echo $fecha;
-                
-				$contaor = 0;
-				if (isset($_POST['fechaf'])) {
-					
+// Establecer la zona horaria a Colombia
+date_default_timezone_set('America/Bogota');
 
-					$ff = $_POST['fechaf'];
-					
-					$formatoValido = '/^\d{4}-\d{2}-\d{2}$/';
-					
-					 $fechaf = preg_match($formatoValido,$ff);
-			
-			       // $sql =  "SELECT SUM(valor_cobrado) AS total FROM moto WHERE   fecha_salida >= "."'$fi'"." AND fecha_salida <=  "."'$ff'";
-			         $sql ="SELECT SUM(valor_cobrado) AS total FROM moto WHERE fecha_salida >= '$ff' AND fecha_salida < DATE_ADD('$ff', INTERVAL 1 DAY);";
-					 //echo $sql;
-					 $totalParqueadero = mysqli_query($conn,	$sql);
-					 $filapar = mysqli_fetch_assoc($totalParqueadero);
-					 $totalParqueadero2 = $filapar['total'];
- 
- 
-					 $totalLavadas = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS  totallavadas FROM lavadas WHERE  fecha_salida >= '$ff' AND fecha_salida < DATE_ADD('$ff', INTERVAL 1 DAY);");
-					 $filalav= mysqli_fetch_assoc($totalLavadas);
-					 $totalLavadas = $filalav['totallavadas'];
- 
-					 
-					 $sqlingresos = "SELECT SUM(valor) AS total FROM ingresos WHERE fecha >= '$ff' AND fecha < DATE_ADD('$ff', INTERVAL 1 DAY);";
-					 //echo $sqlingresos;
-					 $totalIngresos = mysqli_query($conn,$sqlingresos );
-					 $filain = mysqli_fetch_assoc($totalIngresos);
-					 $totalIngresos2 = $filain['total'];
- 
- 
-					  $totalEgresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM egresos WHERE fecha >='$ff' AND fecha < DATE_ADD('$ff', INTERVAL 1 DAY);");
-					  $filae = mysqli_fetch_assoc($totalEgresos);
-					  $totalEgresos2 = $filae['total'];
- 
-					} else {
+$dia = date('d'); // Día actual (en formato de dos dígitos, con ceros iniciales si es necesario)
+$mes = date('m'); // Mes actual (en formato de dos dígitos, con ceros iniciales si es necesario)
+$ano = date('Y'); // Año actual (en formato de cuatro dígitos)
 
-					$totalParqueadero = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS total FROM moto WHERE DATE(fecha_salida)= '$fecha';");
-					$filapar = mysqli_fetch_assoc($totalParqueadero);
-					$totalParqueadero2 = $filapar['total'];
+$fecha = $ano . "-" . $mes . "-" . $dia;
+//echo $fecha;
 
+$contaor = 0;
+if (isset($_POST['fechai']) && isset($_POST['fechaf'])) {
 
-					$totalLavadas = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS totallavadas FROM lavadas WHERE DATE(fecha_salida)=  '$fecha';");
-					$filalav = mysqli_fetch_assoc($totalLavadas);
-					$totalLavadas = $filalav['totallavadas'];
+    $ff = $_POST['fechaf'];
+	
+    $fi = $_POST['fechai'];
 
+    $formatoValido = '/^\d{4}-\d{2}-\d{2}$/';
 
-					 $totalIngresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM ingresos WHERE DATE(fecha) =  '$fecha';");
-					 $filain = mysqli_fetch_assoc($totalIngresos);
-					 $totalIngresos2 = $filain['total'];
+    $fechaf = preg_match($formatoValido, $ff);
+	
+    $fechafi = preg_match($formatoValido, $fi);
 
-					 $totalEgresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM egresos WHERE DATE(fecha) =  '$fecha';");
-					 $filae = mysqli_fetch_assoc($totalEgresos);
-					 $totalEgresos2 = $filae['total'];
+    // $sql =  "SELECT SUM(valor_cobrado) AS total FROM moto WHERE   fecha_salida >= "."'$fi'"." AND fecha_salida <=  "."'$ff'";
+    $sql = "SELECT SUM(valor_cobrado) AS total FROM moto WHERE fecha_salida >= '$fi' AND fecha_salida < DATE_ADD('$ff', INTERVAL 1 DAY);";
+    //echo $sql;
+    $totalParqueadero = mysqli_query($conn, $sql);
+    $filapar = mysqli_fetch_assoc($totalParqueadero);
+    $totalParqueadero2 = $filapar['total'];
 
-					}
+    $totalLavadas = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS  totallavadas FROM lavadas WHERE  fecha_salida >= '$fi' AND fecha_salida < DATE_ADD('$ff', INTERVAL 1 DAY);");
+    $filalav = mysqli_fetch_assoc($totalLavadas);
+    $totalLavadas = $filalav['totallavadas'];
 
-					//egresos
+    $sqlingresos = "SELECT SUM(valor) AS total FROM ingresos WHERE fecha >= '$fi' AND fecha < DATE_ADD('$ff', INTERVAL 1 DAY);";
+    //echo $sqlingresos;
+    $totalIngresos = mysqli_query($conn, $sqlingresos);
+    $filain = mysqli_fetch_assoc($totalIngresos);
+    $totalIngresos2 = $filain['total'];
 
+    $totalEgresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM egresos WHERE fecha >='$fi' AND fecha < DATE_ADD('$ff', INTERVAL 1 DAY);");
+    $filae = mysqli_fetch_assoc($totalEgresos);
+    $totalEgresos2 = $filae['total'];
 
-				?>
+} else {
+
+    $totalParqueadero = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS total FROM moto WHERE DATE(fecha_salida)= '$fecha';");
+    $filapar = mysqli_fetch_assoc($totalParqueadero);
+    $totalParqueadero2 = $filapar['total'];
+
+    $totalLavadas = mysqli_query($conn, "SELECT SUM(valor_cobrado) AS totallavadas FROM lavadas WHERE DATE(fecha_salida)=  '$fecha';");
+    $filalav = mysqli_fetch_assoc($totalLavadas);
+    $totalLavadas = $filalav['totallavadas'];
+
+    $totalIngresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM ingresos WHERE DATE(fecha) =  '$fecha';");
+    $filain = mysqli_fetch_assoc($totalIngresos);
+    $totalIngresos2 = $filain['total'];
+
+    $totalEgresos = mysqli_query($conn, "SELECT SUM(valor) AS total FROM egresos WHERE DATE(fecha) =  '$fecha';");
+    $filae = mysqli_fetch_assoc($totalEgresos);
+    $totalEgresos2 = $filae['total'];
+
+}
+
+//egresos
+
+?>
 
 			<table class="table table-striped table-bordered table-hover">
 				<thead>
@@ -164,36 +166,36 @@
 					<th>Ingresos</th>
 					<th>Gastos</th>
 					<th>Total</th>
-					
+
 				</thead>
 				<tbody>
 					<tr>
 						<td>
 							<?php
-							if (isset($_POST['fechaf'])) {
-								echo $_POST['fechaf'];
-							}else{
-								echo $fecha;
-							}
+if (isset($_POST['fechai']) && isset($_POST['fechaf'])) {
+    echo $_POST['fechai']." / ". $_POST['fechaf'];
+} else {
+    echo $fecha;
+}
 
-							?>
+?>
 						</td>
-						<td> <?php echo "$".number_format((int)$totalParqueadero2,0,',','.');?> </td>
-						<td> <?php echo "$".number_format((int)$totalLavadas,0,',','.');?> </td>
-						<td> <?php echo "$".number_format((int)$totalIngresos2,0,',','.'); ?> </td>
-						<td> <?php echo "$".number_format((int)$totalEgresos2,0,',','.'); ?> </td>
-						
+						<td> <?php echo "$" . number_format((int) $totalParqueadero2, 0, ',', '.'); ?> </td>
+						<td> <?php echo "$" . number_format((int) $totalLavadas, 0, ',', '.'); ?> </td>
+						<td> <?php echo "$" . number_format((int) $totalIngresos2, 0, ',', '.'); ?> </td>
+						<td> <?php echo "$" . number_format((int) $totalEgresos2, 0, ',', '.'); ?> </td>
+
 						<td>
-							<?php 
-								$totaldia = (($totalParqueadero2+$totalLavadas+$totalIngresos2)-$totalEgresos2);
-							
-								echo "$".number_format((int)$totaldia,0,',','.'); 
-							?> 
+							<?php
+$totaldia = (($totalParqueadero2 + $totalLavadas + $totalIngresos2) - $totalEgresos2);
+
+echo "$" . number_format((int) $totaldia, 0, ',', '.');
+?>
 						</td>
 						</tr>
 				</tbody>
 			</table>
-			
+
 		</div>
 
 
